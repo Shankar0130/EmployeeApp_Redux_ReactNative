cdbb7beeb6f14ee9827a5bdcf2f140cb2cfbf38a
shankaryadav <shankaryadav0130@gmail.com>
2021-04-27 23:41:23 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux';
+import EmployeeApp from './src/EmployeeApp';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const initialState = {
+  1: {
+    empid: 1,
+    empName: "Employee One",
+    empSalary: 10000.0
+  },
+  2: {
+    empid: 2,
+    empName: "Employee Tow",
+    empSalary: 20000.0
+  },
+  3: {
+    empid: 3,
+    empName: "Employee Three",
+    empSalary: 30000.0
+  },
+  4: {
+    empid: 4,
+    empName: "Employee Four",
+    empSalary: 40000.0
+  },
+  5: {
+    empid: 5,
+    empName: "Employee Five",
+    empSalary: 50000.0
+  }
+};
+
+const reducer = (state = initialState, action) => {
+  let initialSalary;
+  let increment;
+  let newSalary;
+  let employeeObject;
+
+  if (action.id) {
+    initialSalary = state[action.id].empSalary;
+    increment = (initialSalary / 100) * 20;
+  }
+
+  switch (action.type) {
+    case "GOOD_PERFORMANCE":
+      newSalary = initialSalary + increment;
+      employeeObject = state[action.id];
+      employeeObject.empSalary = newSalary;
+      //return new state
+      return Object.assign({}, state);
+
+    case "BAD_PERFORMANCE":
+      newSalary = initialSalary - increment;
+      employeeObject = state[action.id];
+      employeeObject.empSalary = newSalary;
+      //return new state
+      return Object.assign({}, state);
+  }
+  return state;
+};
+
+var store = createStore(reducer);
+
+export default class App extends React.Component {
+  
+  render() {
+    
+    return (
+      <Provider  store={store}>
+        <EmployeeApp />
+      </Provider>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Readme.md
@@ -0,0 +1,3 @@
+# EmployeeApp in React Native
+
+![Image Not Found](https://i.imgur.com/znl4J4w.jpg)
